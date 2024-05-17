1dbecc5251758d4a35ef32ad1cc179fe69cb3649
abin-1287 <77778475+abin-1287@users.noreply.github.com>
2024-05-17 12:17:00 +0800

    Add files via upload

---
+++ models/r2gen.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+from modules.visual_extractor import VisualExtractor
+from modules.encoder_decoder import EncoderDecoder
+
+
+class R2GenModel(nn.Module):
+    def __init__(self, args, tokenizer):
+        super(R2GenModel, self).__init__()
+        self.args = args
+        self.tokenizer = tokenizer
+        self.visual_extractor = VisualExtractor(args)
+        self.encoder_decoder = EncoderDecoder(args, tokenizer)
+        self.Linear = nn.Linear(18, 9)
+        if args.dataset_name == 'iu_xray':
+            self.forward = self.forward_iu_xray
+        else:
+            self.forward = self.forward_mimic_cxr
+
+    def __str__(self):
+        model_parameters = filter(lambda p: p.requires_grad, self.parameters())
+        params = sum([np.prod(p.size()) for p in model_parameters])
+        return super().__str__() + '\nTrainable parameters: {}'.format(params)
+
+    def forward_iu_xray(self, images, targets=None, mode='train'):
+        att_feats_0, fc_feats_0, images_feature0 = self.visual_extractor(images[:, 0])
+        att_feats_1, fc_feats_1, images_feature1 = self.visual_extractor(images[:, 1])
+        fc_feats = torch.cat((fc_feats_0, fc_feats_1), dim=1)
+        att_feats = torch.cat((att_feats_0, att_feats_1), dim=1)
+        images_feature = torch.cat((images_feature0, images_feature1), dim=1)
+        images_feature = self.Linear(images_feature)
+        if mode == 'train':
+            output = self.encoder_decoder(fc_feats, att_feats, targets, mode='forward')
+        elif mode == 'sample':
+            output, _ = self.encoder_decoder(fc_feats, att_feats, mode='sample')
+        else:
+            raise ValueError
+        return output, images_feature
+
+    def forward_mimic_cxr(self, images, targets=None, mode='train'):
+        att_feats, fc_feats, images_feature = self.visual_extractor(images)
+        if mode == 'train':
+            output = self.encoder_decoder(fc_feats, att_feats, targets, mode='forward')
+        elif mode == 'sample':
+            output, _ = self.encoder_decoder(fc_feats, att_feats, mode='sample')
+        else:
+            raise ValueError
+        return output, images_feature
+
